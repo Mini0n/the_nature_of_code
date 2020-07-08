12fdac79f6cf8786e372e93a727502b4b75b46c0
Eduardo PL <mini0n.000@gmail.com>
2020-07-08 01:52:08 -0500

    updates

--- physEngine/random.js
+++ physEngine/random.js
@@ -10,7 +10,7 @@ var walker;
 
 function setup() {
   // Sets the screen to be 720 pixels wide and 400 pixels high
-  createCanvas(720, 400);
+  createCanvas(500, 500);
   walker = new Walker();
 }
 
@@ -22,7 +22,7 @@ function draw() {
 }
 
 function Walker() {
-  this.pos = createVector(width / 2, height / 2);
+  this.pos = createVector(0, 0);
   this.vel = createVector(0, 0);
   this.acc = createVector(0.1, 0.1);
 
@@ -35,7 +35,7 @@ function Walker() {
   }
 
   function updateFun() {
-    this.vel = this.vel.add(this.acc);
-    this.pos = this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
   }
 }

d631e3a62a2c7322b840ef3a651f3b23c442670f
Eduardo PL <mini0n.000@gmail.com>
2020-07-08 01:46:21 -0500

    falling ball

---
+++ physEngine/random.js
@@ -0,0 +1,41 @@
+//
+//
+//
+//
+//
+//
+//
+
+var walker;
+
+function setup() {
+  // Sets the screen to be 720 pixels wide and 400 pixels high
+  createCanvas(720, 400);
+  walker = new Walker();
+}
+
+function draw() {
+  background("#050505");
+
+  walker.update();
+  walker.display();
+}
+
+function Walker() {
+  this.pos = createVector(width / 2, height / 2);
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0.1, 0.1);
+
+  this.display = walkerDisplay;
+  this.update = updateFun;
+
+  function walkerDisplay() {
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, 20, 20);
+  }
+
+  function updateFun() {
+    this.vel = this.vel.add(this.acc);
+    this.pos = this.pos.add(this.vel);
+  }
+}
